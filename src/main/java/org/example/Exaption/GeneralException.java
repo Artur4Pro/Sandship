7745ebfc34f180c386d727386b69cc877dbae243
Artur <arthur.hayrapetyann@gmail.com>
2024-04-16 17:39:22 +0400

    First

---
+++ src/main/java/org/example/Exaption/GeneralException.java
@@ -0,0 +1,7 @@
+package org.example.Exaption;
+
+public class GeneralException extends RuntimeException {
+    public GeneralException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/example/Exaptions/GeneralExceptions.java
+++
@@ -1,7 +0,0 @@
-package org.example.Exaptions;
-
-public class GeneralExceptions extends RuntimeException {
-    public GeneralExceptions(String message) {
-        super(message);
-    }
-}
